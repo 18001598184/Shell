f5679b3df6fb22f0383a51b6024a08b969d17056
18001598184 <41778471+18001598184@users.noreply.github.com>
2019-08-30 14:42:03 +0800

    Create Topic_LAG.sh

---
+++ Topic_LAG.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+source /etc/profile
+cd /home/kafka/
+KAFKA_GROUP=`bin/kafka-consumer-groups.sh --bootstrap-server 127.0.0.1:9092 --list`
+kafka-consumer-groups.sh --bootstrap-server 127.0.0.1:9092 --group $KAFKA_GROUP --describe >topic.log
+i=0
+while read -r line
+do
+let i=i+1
+LAG=`echo $line |awk '{print $5}'`
+topic_name=`echo $line |awk '{print $1}'`
+if [[ $i > 2 ]]&&[[ $LAG >1 ]];then
+echo $topic_name -- $LAG
+fi
+done<topic.log
